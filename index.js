0e4861347d3948976d631b8c9bf457af9df2ff21
Anuanushree <anuabi2408@gmail.com>
2023-07-12 09:54:24 +0530

    file created

--- index.js
+++ index.js
@@ -1,82 +1,17 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
+const router = require('./router/router');
 const cors = require('cors');
 
 // middlewhere
-
 app.use(cors);
 app.use(express.json());
 
-
-// endpoints and  / route
 app.get('/', (request, response) => {
-
-    response.send('successfully inserted')
-    // response.status(201).json({ message: 'node created successfullt successfully' })
+    response.send('to create a file give the path name as /create')
 })
-app.use('/file',route)
-// // to get all the notes
-// app.get('/api/notes', (request, response) => {
-//     response.status(200).json(notes);
-// });
-
-// // post endpoint
-
-// app.post('/api/notes', (request, response) => {
-//     notes = notes.concat(request.body);
-//     response.status(201).json({ message: 'node created successfullt successfully' })
-// })
-
-// // fetch a single resource based on id
-
-// app.get('/api/notes/:id', (request, response) => {
-//     const id = request.params.id;
-//     const note = notes.find(note => note.id == id);
-//     if (note) {
-//         response.status(200).json(note);
-//     } else {
-//         response.status(404).json({ message: 'id does not exists' })
-//     }
-
-// })
-
-// app.delete('/api/notes/:id', (request, response) => {
-//     const id = request.params.id;
-//     const note = notes.find(note => note.id == id);
-//     notes = notes.filter(note => note.id != id)
-//     if (note) {
-//         response.status(200).json(note);
-//     } else {
-//         response.status(404).json({ message: 'id does not exists' })
-//     }
-// });
-
-// app.put('/api/notes/:id', (request, response) => {
-//     const id = request.params.id;
-//     const notereplace = request.body;
-//     const note = notes.find(note => note.id == id);
-//     notes = notes.map(note => note.id == id ? notereplace : note);
-
-//     if (note) {
-//         response.status(200).json({ message: 'note replaced' });
-//     } else {
-//         response.status(404).json({ message: 'id does not find' })
-//     }
-// });
-
-// app.patch('/api/notes/:id', (request, response) => {
-//     const id = request.params.id;
-//     const notereplace = request.body;
-//     const note = notes.find(note => note.id == id);
-//     notes = notes.map(note => note.id == id ? { ...note, ...notereplace } : note);
-
-//     if (note) {
-//         response.status(200).json({ message: 'note replaced' });
-//     } else {
-//         response.status(404).json({ message: 'id does not find' })
-//     }
-// });
+app.use(router);
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`server running port ${PORT}`);
--- router/router.js
+++ router/router.js
@@ -1,27 +1,53 @@
-const expresss= require('express');
+const express = require('express');
 const router = express.Router();
-
-const date = new Date()
-const currentDate = new Date().toLocaleDateString("de-DE");
-let currentTime = date.getHours() + "-" + date.getMinutes() + "-" + date.getSeconds();
-console.log(currentDate);
-console.log(currentTime);
 const fs = require('fs');
 
-router.get("/create",async(request,response)=>{
-    try{
-        fs.writeFile(`/Users/anuab/OneDrive/Document/react-be/testdir/${currentDate} (${currentTime}).txt`, `current date ${currentDate} and time ${currentTime}`, err => {
+
+router.get("/create", async (request, response) => {
+    try {
+        const date = new Date()
+        const currentDate = new Date().toLocaleDateString("de-DE");
+        let currentTime = date.getHours() + "-" + date.getMinutes() + "-" + date.getSeconds();
+        console.log(currentDate);
+        console.log(currentTime);
+        fs.writeFile(`/Users/anuab/OneDrive/Documents/react-be/testdir/${currentDate} (${currentTime}).txt`, `current date ${currentDate} and time ${currentTime}`, err => {
             if (err) {
                 console.error(err);
                 return;
             }
             console.log('content written in the file')
         });
-    }catch{
-        res.send({
+        response.send("file created")
+    } catch {
+        response.send({
             statusCode: 500,
             msg: "Internal server error"
         });
     }
 })
-export default router;
+
+router.get('/list', (request, response) => {
+    try {
+        const path = require('path');
+        let fileslist = [];
+        const folderName = '/Users/anuab/OneDrive/Documents/react-be/testdir/'
+        fs.readdir(folderName, (err, files) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            const extension = '.txt';
+            files.forEach(file => {
+                if (path.extname(file) === extension)
+                    fileslist.push(file);
+            })
+            response.send(fileslist)
+        })
+    } catch {
+        response.send({
+            statusCode: 500,
+            msg: "Internal server error"
+        });
+    }
+})
+module.exports = router; 
