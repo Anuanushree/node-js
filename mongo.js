7dcd38c660cb6e31fa0a8034ddac94f054873f49
Anuanushree <anuabi2408@gmail.com>
2023-07-13 19:47:03 +0530

    try

--- mongo.js
+++ mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb+srv://anushree:Nsaatf4VgEFY6VtY@cluster0.f6ma1cw.mongodb.net/NotesDb?retryWrites=true&w=majority';
+const url = 'mongodb+srv://anushree:Nsaatf4VgEFY6VtY@cluster0.f6ma1cw.mongodb.net/Student-Mentor?retryWrites=true&w=majority';
 
 mongoose.connect(url)
     .then(() => {
@@ -10,36 +10,23 @@ mongoose.connect(url)
         console.error(err);
     })
 
-const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
+const Mentor = new mongoose.Schema({
+    id: Number,
+    mentorName: String,
+    email:String,
+    student:[mongoose.id]
 });
 
-const Note = mongoose.model('Note', noteSchema, 'note');
+const Note = mongoose.model('Note', Mentor, 'note');
 
 const note = new Note({
-    content: 'mongodb connection using mongoo libarray',
-    important: false,
+    id: 1,
+    mentorName:"sathish",
+    email: "sathish@gmail.com",
+    student: [mongoose.Schema.Types.id]
 });
 
-let notes = [
-    {
-        content: 'data1',
-        important: false,
-    },
-    {
-        content: 'data2',
-        important: false,
-    },
-];
-notes.forEach(note => {
-    let noteModel = new Note(note);
 
-    noteModel.save()
-        .then(() => {
-            console.log('note saved');
-        })
-})
 note.save()
     .then((result) => {
         console.log('note saved');
