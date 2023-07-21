40eafa1c84eb1b17ea6a6d3665d02d8ece59e2bf
Anuanushree <anuabi2408@gmail.com>
2023-07-21 23:11:45 +0530

    forgot password

--- controllers/reset.js
+++ controllers/reset.js
@@ -5,38 +5,43 @@ const nodemailer = require('nodemailer');
 const bcrypt = require('bcrypt');
 
 resetRouter.post('/', async (request, response) => {
-    const { email } = request.body;
+    try {
+        const { email } = request.body;
 
-    const user = await User.findOne({ email });
+        const user = await User.findOne({ email });
 
-    if (!user)
-        return response.status(404).json({ error: "user not found" });
+        if (!user)
+            return response.status(404).json({ error: "user not found" });
 
-    const randomstring = Math.random().toString(20).substring(4, 15)
+        const randomstring = Math.random().toString(20).substring(4, 15)
 
 
-    const link = `http://localhost:3000/register/${randomstring}`;
+        const link = `https://silver-concha-7e61b5.netlify.app/reset/${randomstring}`;
 
-    user.resetToken = randomstring;
-    await user.save();
+        user.resetToken = randomstring;
+        await user.save();
 
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: config.EMAIL,
-            pass: config.PASS
-        }
-    });
-    async function sendMail() {
-        const info = await transporter.sendMail({
-            from: `"Anushree" <${config.EMAIL}>`,
-            to: user.email,
-            subject: "Reset password",
-            text: link,
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: config.EMAIL,
+                pass: config.PASS
+            }
         });
-        console.log("message send:%s", info.messageId);
+        async function sendMail() {
+            const info = await transporter.sendMail({
+                from: `"Anushree" <${config.EMAIL}>`,
+                to: user.email,
+                subject: "Reset password",
+                text: link,
+            });
+            console.log("message send:%s", info.messageId);
 
+        }
+        sendMail().catch(console.error);
+        response.status(200).json({ substring: randomstring, user: user })
+    } catch (error) {
+        response.status(200).json({ message: "user not found" })
     }
-    sendMail().catch(console.error);
 })
 module.exports = resetRouter;
